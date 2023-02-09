717d4580c5722a9d18f70363ff2cd2d71362e843
idevF <124703341+idevF@users.noreply.github.com>
2023-02-09 14:34:57 +0300

    Initial Commit

---
+++ LittleLemonRestaurant/LittleLemonRestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonRestaurantApp.swift
+//  LittleLemonRestaurant
+//
+//  Created by idevF on 9.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonRestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
