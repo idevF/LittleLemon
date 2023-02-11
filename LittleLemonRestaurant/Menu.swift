403209bc2613200c89b9e4b8a8c58df5bc519a21
idevF <124703341+idevF@users.noreply.github.com>
2023-02-11 12:38:48 +0300

    Database model file added and server query parsing data displaying the fetched data related codes added

--- LittleLemonRestaurant/Menu.swift
+++ LittleLemonRestaurant/Menu.swift
@@ -16,19 +16,35 @@ struct Menu: View {
             Text("Chicago")
             Text("We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.")
             
-            List {
-                
+            FetchedObjects() { (dishes: [Dish]) in
+                List {
+                    ForEach(dishes) { dish in
+                        HStack {
+                            Text(dish.title ?? "")
+                            Text(dish.price ?? "")
+                            
+                            AsyncImage(url: URL(string: dish.image ?? "")) { image in
+                                 image.resizable()
+                            } placeholder: {
+                                ProgressView()
+                            }
+                            .frame(width: 80, height: 80)
+                        }
+                    }
+                }
             }
+            
         }
         .onAppear {
             getMenuData()
         }
     }
     
-    func getMenuData() {
+    private func getMenuData() {
+        PersistenceController.shared.clear()
+
         let menuAddress = "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json"
         let url = URL(string: menuAddress)!
-        // guard let url = url else { throw NSError() }
         
         let request = URLRequest(url: url)
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
@@ -36,6 +52,16 @@ struct Menu: View {
                 print(string)
                 let decoder = JSONDecoder()
                 let fullMenu = try? decoder.decode(MenuList.self, from: data)
+                //print(fullMenu?.menu)
+                for menuItem in fullMenu!.menu {
+                    let newDish = Dish(context: viewContext)
+                    newDish.title = menuItem.title
+                    newDish.image = menuItem.image
+                    newDish.category = menuItem.category
+                    newDish.explanation = menuItem.explanation
+                    newDish.price = menuItem.price
+                }
+                try? viewContext.save()
             }
         }
         task.resume()
--- LittleLemonRestaurant/MenuItem.swift
+++ LittleLemonRestaurant/MenuItem.swift
@@ -9,16 +9,16 @@ import Foundation
 
 struct MenuItem: Decodable {
     let title: String
-    let description: String
-    let price: String
     let image: String
+    let price: String
+    let explanation: String
     let category: String
     
     enum CodingKeys: String, CodingKey {
         case title = "title"
-        case description = "description"
-        case price = "price"
         case image = "image"
+        case price = "price"
+        case explanation = "description"
         case category = "category"
     }
 }
